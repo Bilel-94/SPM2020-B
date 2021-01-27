d17e79e6b286d55c44db73bd38755e0a0f397036
Bilel-94 <72302448+Bilel-94@users.noreply.github.com>
2021-01-27 17:02:19 +0200

    Add New Structure

---
+++ Park-in/Park-in/src/main/java/SPMB/Parkin/ParkInApplication.java
@@ -0,0 +1,13 @@
+package SPMB.Parkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkInApplication.class, args);
+	}
+
+}
