9be030aeac61a9998ceabc6b2af88035959526fb
Bilel-94 <72302448+Bilel-94@users.noreply.github.com>
2021-01-27 17:20:14 +0200

    Add backend structure

---
+++ backend/Park-in/Park-in/src/test/java/SPMB/Parkin/ParkInApplicationTests.java
@@ -0,0 +1,13 @@
+package SPMB.Parkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
