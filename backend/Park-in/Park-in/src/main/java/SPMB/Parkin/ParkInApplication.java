53a203034d990beecefaff60e478bd1f75e6ce8a
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-05 18:18:03 +0200

    Add ParkInApplication

--- backend/Park-in/Park-in/src/main/java/SPMB/Parkin/ParkInApplication.java
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/ParkInApplication.java
@@ -1,13 +1,25 @@
 package SPMB.Parkin;
 
+import SPMB.Parkin.enums.Roles;
+import SPMB.Parkin.models.User;
+import SPMB.Parkin.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class ParkInApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkInApplication.class, args);
-	}
+    @Autowired
+    private UserRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkInApplication.class, args);
+    }
+
+
 
 }
