c1cce1e72a1486d8aa5da790a44e748566582010
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-05 16:55:56 +0200

    Add User Model

---
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/models/User.java
@@ -0,0 +1,44 @@
+package SPMB.Parkin.models;
+
+import SPMB.Parkin.enums.Roles;
+import org.springframework.data.annotation.Id;
+
+public class User {
+    @Id
+    private String id;
+
+    private String username;
+    private String hashedPassword;
+    private Roles role;
+
+    public User(String username, String hashedPassword, Roles role) {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.setRole(role);
+    }
+
+
+
+
+    /*-----------ACCESSORY METHODS--------------*/
+    public String getUserName() {
+        return username;
+    }
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+    public void setRole(Roles role) {
+        this.role = role;
+    }
+}
