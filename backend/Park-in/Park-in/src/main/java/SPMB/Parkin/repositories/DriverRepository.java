8dfb27efd8a3cb93b9bb181e5be9dbd9cfc3765e
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-05 18:11:47 +0200

    Add DriverRepository

---
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/repositories/DriverRepository.java
@@ -0,0 +1,9 @@
+package SPMB.Parkin.repositories;
+
+import SPMB.Parkin.models.Driver;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DriverRepository extends MongoRepository<Driver, Driver> {
+
+
+}
