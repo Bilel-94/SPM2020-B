9a4f8b3ae7b0708ef8b6a38a88331ebd9045ffd2
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-19 12:11:32 +0200

    Modified JwtResponse and AuthController according to Endpoint

--- backend/Park-in/Park-in/src/main/java/SPMB/Parkin/controllers/AuthController.java
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/controllers/AuthController.java
@@ -1,7 +1,11 @@
 package SPMB.Parkin.controllers;
 
-import SPMB.Parkin.models.Credentials;
+import SPMB.Parkin.enums.Roles;
+import SPMB.Parkin.models.DriverInfo;
+import SPMB.Parkin.models.User;
+import SPMB.Parkin.models.requestBody.Credentials;
 import SPMB.Parkin.payload.reponse.JwtResponse;
+import SPMB.Parkin.repositories.DriverInfoRepository;
 import SPMB.Parkin.repositories.UserRepository;
 import SPMB.Parkin.security.jwt.JwtUtils;
 import SPMB.Parkin.security.service.UserDetailsImpl;
@@ -10,17 +14,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static SPMB.Parkin.constants.EndpointConstants.AUTH_ENDPOINT;
+import static SPMB.Parkin.constants.EndpointConstants.LOGIN_ENDPOINT;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping(AUTH_ENDPOINT)
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
@@ -29,25 +33,30 @@ public class AuthController {
     UserRepository userRepository;
 
     @Autowired
+    DriverInfoRepository driverInfoRepository;
+
+    @Autowired
     PasswordEncoder encoder;
 
     @Autowired
     JwtUtils jwtUtils;
 
-    @PostMapping("/login")
+    @PostMapping(LOGIN_ENDPOINT)
     public ResponseEntity<?> authenticateUser(@RequestBody Credentials credentials) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(credentials.getEmail(), credentials.getPassword()));
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtToken(authentication);
-
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+        User user = userRepository.findByUsername(userDetails.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + userDetails.getEmail()));
+        JwtResponse response = new JwtResponse(jwt, user);
+        if(user.getRole() == Roles.ROLE_DRIVER) {
+            DriverInfo driverInfo = driverInfoRepository.findByUsername(user.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Driver Not Found with username: " + userDetails.getEmail()));
+            response.setPlate(driverInfo.getPlate());
+            response.setVehicleType(driverInfo.getVehicleType());
+            response.setIsHandicap(driverInfo.getHandicap());
+        }
+        return ResponseEntity.ok(response);
     }
 
 }
--- backend/Park-in/Park-in/src/main/java/SPMB/Parkin/payload/reponse/JwtResponse.java
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/payload/reponse/JwtResponse.java
@@ -1,20 +1,36 @@
 package SPMB.Parkin.payload.reponse;
 
-import java.util.List;
+import SPMB.Parkin.models.User;
 
 public class JwtResponse {
+
     private String token;
     private String id;
     private String username;
     private String email;
-    private List<String> roles;
+    private String ssn;
+    private String phone;
+    private String firstName;
+    private String lastName;
+    private String plate;
+    private String vehicleType;
+    private Boolean isHandicap;
 
-    public JwtResponse(String token, String id, String username, String email, List<String> roles) {
+    private String role;
+
+    public JwtResponse(String token, User user) {
         this.setToken(token);
-        this.setId(id);
-        this.setUsername(username);
-        this.setEmail(email);
-        this.setRoles(roles);
+        this.setId(user.getId());
+        this.setUsername(user.getUsername());
+        this.setEmail(user.getEmail());
+        this.setSsn(user.getSsn());
+        this.setFirstName(user.getFirstName());
+        this.setLastName(user.getLastName());
+        this.setPhone(user.getPhone());
+        this.setRoles(user.getRole().name());
+        this.plate = null;
+        this.vehicleType = null;
+        this.setIsHandicap(null);
     }
 
     public String getToken() {
@@ -49,11 +65,67 @@ public class JwtResponse {
         this.email = email;
     }
 
-    public List<String> getRoles() {
-        return roles;
+    public String getRoles() {
+        return role;
     }
 
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
+    public void setRoles(String role) {
+        this.role = role;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public Boolean getIsHandicap() {
+        return isHandicap;
+    }
+
+    public void setIsHandicap(Boolean isHandicap) {
+        this.isHandicap = isHandicap;
     }
 }
