7a7eebb80e46cb371a4f308ec60bf9291d235da8
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-19 12:02:48 +0200

    Add EndpointConstants to manege driver an authentication controllers

---
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/constants/EndpointConstants.java
@@ -0,0 +1,11 @@
+package SPMB.Parkin.constants;
+
+public class EndpointConstants {
+    //authentication controller
+    public static final String AUTH_ENDPOINT = "/api/auth";
+    public static final String LOGIN_ENDPOINT = "/login";
+
+    //driver controller
+    public static final String DRIVER_ENDPOINT = "/api/driver";
+    public static final String DRIVER_REGISTRATION_ENDPOINT = "/registration";
+}
