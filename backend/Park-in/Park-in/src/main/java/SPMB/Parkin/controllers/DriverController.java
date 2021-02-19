e019a3eba67253e571630887631d93af0451667e
Bilel <bilel.braiek@studenti.unicam.it>
2021-02-19 12:19:23 +0200

    Modified DriverController according to Endpoint

--- backend/Park-in/Park-in/src/main/java/SPMB/Parkin/controllers/DriverController.java
+++ backend/Park-in/Park-in/src/main/java/SPMB/Parkin/controllers/DriverController.java
@@ -1,30 +1,48 @@
 package SPMB.Parkin.controllers;
 
+import SPMB.Parkin.enums.Roles;
 import SPMB.Parkin.models.Driver;
-import SPMB.Parkin.repositories.DriverRepository;
+import SPMB.Parkin.models.DriverInfo;
+import SPMB.Parkin.models.User;
+import SPMB.Parkin.repositories.DriverInfoRepository;
+import SPMB.Parkin.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
 import java.io.IOException;
 
+import static SPMB.Parkin.constants.EndpointConstants.DRIVER_ENDPOINT;
+import static SPMB.Parkin.constants.EndpointConstants.DRIVER_REGISTRATION_ENDPOINT;
+
 @RestController
+@RequestMapping(DRIVER_ENDPOINT)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class DriverController {
 
-
-
+    @Autowired
+    private UserRepository repository;
 
     @Autowired
-    private DriverRepository repository;
+    private DriverInfoRepository driverRepository;
+    @Autowired
+    PasswordEncoder encoder;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping(path = DRIVER_REGISTRATION_ENDPOINT, consumes = "application/json")
+    public ResponseEntity registration(@Valid @RequestBody Driver driver) throws IOException {
 
+        if (!repository.existsByUsername(driver.getEmail())) {
+            repository.save(new User(driver.getUsername(), driver.getFirstName(), driver.getLastName(), driver.getSsn(),
+                    driver.getPhone(), driver.getEmail(), encoder.encode(driver.getPassword()), Roles.ROLE_DRIVER));
+            driverRepository.save(new DriverInfo(driver));
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
 
-    @PostMapping(path="api/registration",consumes = "application/json" )
-    public void registration(@Valid @RequestBody Driver driver) throws IOException {
-        repository.save(driver);
     }
 }
